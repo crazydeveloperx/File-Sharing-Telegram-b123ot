f04324a3eeff1ab5208357b99a8b1dcf8038b5dc
Harshit07s <harshitnarang624@gmail.com>
2025-03-20 07:19:34 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -13,9 +13,9 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-# @Bot.on_message(filters.private & ~filters.user(ADMINS) & ~filters.command(['start','ping', 'ch2l', 'cancel']))
+# @Bot.on_message(filters.private & ~filters.user(ADMINS) & ~filters.command(['start','ping', 'ch2l', 'cancel','auth']))
 # @Bot.on_message(filters.private & filters.incoming)
 # async def useless(_, message: Message):
-    # if USER_REPLY_TEXT:
+    # if USER_REPLY_TEXT:p
        # await message.reply(USER_REPLY_TEXT)
 
